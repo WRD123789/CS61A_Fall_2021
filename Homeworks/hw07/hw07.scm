a572ad8cd74d7ee64b0174eaddb29b9cc1cd1c60
WRD__ <92314796+WRD123789@users.noreply.github.com>
2022-05-11 21:37:33 +0800

    Add files via upload

---
+++ Homeworks/hw07/hw07.scm
@@ -0,0 +1,38 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) 'YOUR-CODE-HERE)
+
+(define (caddr s) 'YOUR-CODE-HERE)
+
+(define (ordered? s) 'YOUR-CODE-HERE)
+
+(define (square x) (* x x))
+
+(define (pow base exp) 'YOUR-CODE-HERE)
+
+
+;Q1: Thane of Cadr
+(define (cadr s)
+    (car (cdr s)))
+
+(define (caddr s)
+    (car (cddr s)))
+
+
+;Q2: Ordered
+(define (ordered? s)
+    (begin
+        (define (helper s pre)
+            (cond 
+                  ((null? s) #t)
+                  ((> pre (car s)) #f)
+                  (#t (helper (cdr s) (car s)))))
+        (helper s -1e10)))
+
+
+;Q3: Pow
+(define (pow base exp)
+    (cond
+        ((= exp 0) 1)
+        ((= (modulo exp 2) 0) (pow (square base) (/ exp 2)))
+        (#t (* (pow (square base) (quotient exp 2)) base))))
