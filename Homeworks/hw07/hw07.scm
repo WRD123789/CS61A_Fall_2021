ba0bee6549d61223287e66820d3c53d24b13b279
WRD__ <92314796+WRD123789@users.noreply.github.com>
2022-05-12 17:00:04 +0800

    Update hw07.scm

--- Homeworks/hw07/hw07.scm
+++ Homeworks/hw07/hw07.scm
@@ -13,26 +13,30 @@
 
 ;Q1: Thane of Cadr
 (define (cadr s)
-    (car (cdr s)))
+    (car (cdr s))
+)
 
 (define (caddr s)
-    (car (cddr s)))
+    (car (cddr s))
+)
 
 
 ;Q2: Ordered
 (define (ordered? s)
-    (begin
-        (define (helper s pre)
-            (cond 
-                  ((null? s) #t)
-                  ((> pre (car s)) #f)
-                  (#t (helper (cdr s) (car s)))))
-        (helper s -1e10)))
+    (define (helper s pre)
+        (cond ((null? s) #t)
+              ((> pre (car s)) #f)
+              (#t (helper (cdr s) (car s)))
+        )
+    )
+    (helper s -1e10)
+)
 
 
 ;Q3: Pow
 (define (pow base exp)
-    (cond
-        ((= exp 0) 1)
-        ((= (modulo exp 2) 0) (pow (square base) (/ exp 2)))
-        (#t (* (pow (square base) (quotient exp 2)) base))))
+    (cond ((= exp 0) 1)
+          ((= (modulo exp 2) 0) (pow (square base) (/ exp 2)))
+          (#t (* (pow (square base) (quotient exp 2)) base))
+    )
+)
